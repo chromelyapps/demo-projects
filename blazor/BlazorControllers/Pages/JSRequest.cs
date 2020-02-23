38767bd2686d54dbe9766acae7bace61731fa72e
softoille@gmail.com <softoille@gmail.com>
2020-02-23 16:52:51 -0600

    Initial blazor demo commit.

---
+++ blazor/BlazorControllers/Pages/JSRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace BlazorControllers.Pages
+{
+    public class JSRequest
+    {
+        public string Url { get; set; }
+        public IDictionary<string, string> Parameters { get; set; }
+        public object PostData { get; set; }
+
+        public string ToJson()
+        {
+            var options = new JsonSerializerOptions();
+            options.ReadCommentHandling = JsonCommentHandling.Skip;
+            options.AllowTrailingCommas = true;
+
+            return JsonSerializer.Serialize(this, options); ;
+        }
+    }
+}
---
+++ blazor/README.md
@@ -0,0 +1,4 @@
+Work in progress!
+
+![](https://github.com/chromelyapps/Chromely/blob/master/Screenshots/blazor.png)
+
